87d19f52bd88898eff34b92aa0d01ce6177a51ed
Akifumi NAKAMURA <tmpz84@gmail.com>
2022-05-09 19:50:56 +0900

    #issue9: solrスキーマの設定をする機能を追加 (#10)

--- cmd/configrator/main.go
+++ cmd/configrator/main.go
@@ -12,11 +12,14 @@ func main() {
 	fmt.Println("Main function started")
 
 	generateDefaultConfigSet := flag.Bool("generate-default-configset", false, "generate default configset to current directory")
+	setupSolrSchema := flag.Bool("setup-solr", false, "setup solr schema")
 	flag.Parse()
 
 	fmt.Println("generateDefaultConfigSet:", *generateDefaultConfigSet)
 	if *generateDefaultConfigSet == true {
 		kassiscore.GenerateDefaultConfigSet()
+	} else if *setupSolrSchema == true {
+		kassiscore.SetupSolr()
 	} else {
 		kassiscore.CheckConfigAndConnections()
 	}
